83690ab7e1e718f652d5dc9d62c4c8e286a7d03d
aerjayc <aerjay.castaneda@eee.upd.edu.ph>
2019-12-04 11:14:13 +0800

    easier change of filenames

--- formality_to_yelp.py
+++ formality_to_yelp.py
@@ -5,6 +5,7 @@ def form_to_yelp(line, vocab):
 	
 	text = form_list[3]
 	for word in re.split(' ', text):
+
 		if word[-1] == '\n':
 			word = word[:-1]
 		vocab.add(word)
@@ -19,16 +20,19 @@ def form_to_yelp(line, vocab):
 	return mean, text
 
 def main():
-	path_to_formality = 'formality/email'
-	path_to_text = 'formality/modified/email.train.text'
-	path_to_labels = 'formality/modified/email.train.labels'
-	path_to_vocab = 'formality/modified/email.train.vocab'
+	file = 'news'
+	path_to_formality = f'formality/{file}'
+	path_to_text = f'formality/modified/{file}.train.text'
+	path_to_labels = f'formality/modified/{file}.train.labels'
+	path_to_vocab = f'formality/modified/{file}.train.vocab'
 
 	vocab = set()
 
-	with open(path_to_text, 'w') as f:
+	with open(path_to_text, 'w') as ft:
 		pass
-	with open(path_to_labels, 'w') as f:
+	with open(path_to_labels, 'w') as fl:
+		pass
+	with open(path_to_vocab, 'w') as fv:
 		pass
 
 	#import formality text file
@@ -38,11 +42,11 @@ def main():
 			yelped_line = form_to_yelp(line, vocab)
 			print(line)
 
-			with open(path_to_labels, 'a') as f:
-				f.write(yelped_line[0])
-				f.write('\n')
-			with open(path_to_text, 'a') as f:
-				f.write(yelped_line[1])
+			with open(path_to_labels, 'a') as fl:
+				fl.write(yelped_line[0])
+				fl.write('\n')
+			with open(path_to_text, 'a') as ft:
+				ft.write(yelped_line[1])
 
 			line  = ff.readline()
 
